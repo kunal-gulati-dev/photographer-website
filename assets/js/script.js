e94d1c047aebbbe693508723f21cbbba57dc11f5
kunal-gulati-dev <knlgulati365@gmail.com>
2023-05-18 15:44:45 +0530

    added the navbar section

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ window.addEventListener("load", function() {
     loadingElement.classList.add("loaded");
     document.body.classList.remove("active");
 })
+
+
+// mobile nav toggle
+
+const [navTogglers, navLinks, navbar, overlay] = [
+	document.querySelectorAll("[data-nav-toggler]"),
+	document.querySelectorAll("[data-nav-link]"),
+    document.querySelector("[data-navbar]"),
+    document.querySelector("[data-overlay]")
+];
+
+const toggleNav = function () {
+    navbar.classList.toggle("active")
+    overlay.classList.toggle("active")
+    document.body.classList.toggle("active")
+}
+
+addEventOnElements(navTogglers, "click", toggleNav)
